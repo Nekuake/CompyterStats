34dd80cd1867fb51f41002159e3980b358b2c1e7
Nekuake <ruizale55@gmail.com>
2022-09-22 13:04:57 +0200

    Paused Django development and started working in PSUTIL

--- refresh_stats.py
+++ refresh_stats.py
@@ -1,6 +1,17 @@
 import psutil
+import time
+import os
 
 
+def get_cpu_freq():
+    # current, min, max
+    cpu = psutil.cpu_freq()
+    return cpu
 
 
+def get_cpu_load(interval):
+    load_list = psutil.cpu_percent(interval=interval,percpu=True)
+    return load_list
 
+example=get_cpu_load()
+print (example)
