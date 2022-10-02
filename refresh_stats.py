438c0053f0d3a4e658b2779eb4c236f58f9e4fa3
Nekuake <ruizale55@gmail.com>
2022-10-02 13:22:41 +0200

    Overall Disk Usage and per disk

--- refresh_stats.py
+++ refresh_stats.py
@@ -1,5 +1,5 @@
 import json
-
+import logging
 import psutil
 import time
 import os
@@ -8,17 +8,29 @@ import os
 
 def get_cpu_freq():
     # current, min, max
+    logging.info("Getting CPU freq")
     cpu = psutil.cpu_freq()
     return cpu
 
 
 def get_cpu_load(interval):
+    logging.info("Getting CPU load per core")
     load_list = psutil.cpu_percent(interval=interval, percpu=True)
     return load_list
 
+#Windows needs a specific way to calculate CPU usage.
+# Check https://psutil.readthedocs.io/en/latest/#psutil.Process.cpu_percent for more info.
+
+def get_disk_usage(perdisk):
+    disk_usage=psutil.disk_io_counters(perdisk=perdisk)
+    if disk_usage== None or disk_usage=={}:
+        raise Warning("Disk Usage returned None. Diskless system? Returning 0")
+        disk_usage=0
+    return disk_usage
+
 
 def get_process_list():
-    print("Getting processes")
+    logging.info("Getting process list.")
     process_list = []
     for proc in psutil.process_iter():
         try:
@@ -26,20 +38,14 @@ def get_process_list():
             with current_psutil_process.oneshot():
                 temp_process_dict = {
                     "name":             current_psutil_process.name(),
-                    "cpu_percent":      current_psutil_process.cpu_percent()/psutil.cpu_count(), #This one does nothing on Windows due to some psutil limitations.
-                    # Check https://psutil.readthedocs.io/en/latest/#psutil.Process.cpu_percent for more info.
+                    "cpu_percent":      current_psutil_process.cpu_percent()/psutil.cpu_count(),
                     "io_counter":       current_psutil_process.io_counters(),
                     "main_memory_usage":current_psutil_process.memory_info().rss,
                     "virtual_memory_usage": current_psutil_process.memory_info().vms
                 }
-                if(os.name=="nt"):
-                    time.sleep(0.5)
-                    temp_process_dict["cpu_percent"]= current_psutil_process.cpu_percent()/psutil.cpu_count()
                 print(temp_process_dict["name"],":",temp_process_dict["cpu_percent"])
                 process_list.append(temp_process_dict)
         except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
             continue
     return process_list
 
-with open("sample.json", "w") as outfile:
-    json.dump(get_process_list(), outfile)
