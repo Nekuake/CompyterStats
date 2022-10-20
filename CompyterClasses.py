cecdc38d5bb27320c2271ec1d807b8f9119dbae7
Nekuake <ruizale55@gmail.com>
2022-10-20 10:32:16 +0200

    Removed redundant function

--- CompyterClasses.py
+++ CompyterClasses.py
@@ -48,27 +48,6 @@ class Timestap:
 
 
 
-def get_process_list():
-    print("Getting processes")
-    process_list = []
-    for proc in psutil.process_iter():
-        try:
-            current_psutil_process = psutil.Process(proc.pid)
-            with current_psutil_process.oneshot():
-                temp_process_dict = {
-                    "name":             current_psutil_process.name(),
-                    "cpu_percent":      current_psutil_process.cpu_percent()/psutil.cpu_count(), #This one does nothing on Windows due to some psutil limitations.
-                    # Check https://psutil.readthedocs.io/en/latest/#psutil.Process.cpu_percent for more info.
-                    "io_counter":       current_psutil_process.io_counters(),
-                    "main_memory_usage":current_psutil_process.memory_info().rss,
-                    "virtual_memory_usage": current_psutil_process.memory_info().vms
-                }
-                print(temp_process_dict["name"],":",temp_process_dict["cpu_percent"])
-                process_list.append(temp_process_dict)
-        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
-            continue
-    return process_list
 
 
-with open('data.json', 'w') as fp:
-    json.dump(get_process_list(), fp)
+
