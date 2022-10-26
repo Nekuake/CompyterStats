489ee67fe770c3349fb4c4695cf5f5cb225ee244
Nekuake <ruizale55@gmail.com>
2022-10-26 19:54:45 +0200

    Temporal workaround

--- CompyterClasses.py
+++ CompyterClasses.py
@@ -24,27 +24,38 @@ class Timestap:
     def add_process_to_timestamp(self, process):
         self.processes.append(process)
 
-    def get_all_process(self):
-        print("Getting processes")
-        process_list = []
-        for proc in psutil.process_iter():
-            try:
-                current_psutil_process = psutil.Process(proc.pid)
-                with current_psutil_process.oneshot():
-                    temp_process_dict = {
-                        "name": current_psutil_process.name(),
-                        "cpu_percent": current_psutil_process.cpu_percent() / psutil.cpu_count(),
-                        # This one does nothing on Windows due to some psutil limitations.
-                        # Check https://psutil.readthedocs.io/en/latest/#psutil.Process.cpu_percent for more info.
-                        "io_counter": current_psutil_process.io_counters(),
-                        "main_memory_usage": current_psutil_process.memory_info().rss,
-                        "virtual_memory_usage": current_psutil_process.memory_info().vms
-                    }
-                    print(temp_process_dict["name"], ":", temp_process_dict["cpu_percent"])
-                    process_list.append(temp_process_dict)
-                    self.add_processes_to_timestamp(self,temp_process_dict)
-            except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
-                continue
+def get_all_process(processes):
+    print("Getting processes")
+    process_list = []
+    for current_psutil_process in processes:
+        try:
+            with current_psutil_process.oneshot():
+                temp_process_dict = {
+                    "name": current_psutil_process.name(),
+                    "cpu_percent": current_psutil_process.cpu_percent() / psutil.cpu_count(),
+                    # This one does nothing on Windows due to some psutil limitations.
+                    # Check https://psutil.readthedocs.io/en/latest/#psutil.Process.cpu_percent for more info.
+                    "io_counter": current_psutil_process.io_counters(),
+                    "main_memory_usage": current_psutil_process.memory_info().rss,
+                    "virtual_memory_usage": current_psutil_process.memory_info().vms
+                }
+                #print(temp_process_dict["name"], ":", temp_process_dict["cpu_percent"])
+                process_list.append(temp_process_dict)
+                #self.add_processes_to_timestamp(self,temp_process_dict)
+        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+            continue
+    return process_list
+
+processes = [psutil.Process(proc.pid) for proc in psutil.process_iter()]
+
+ls = get_all_process(processes)
+time.sleep(1)
+ls = get_all_process(processes)
+ls.sort(key=lambda x: x['cpu_percent'], reverse=True)
+
+
+with open("sample.json", "w") as outfile:
+    json.dump(ls, outfile)
 
 
 
