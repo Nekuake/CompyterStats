df6151c4d7ee1a44ceb48c1c04d199685abe8ca7
Nekuake <ruizale55@gmail.com>
2022-11-16 00:13:36 +0100

    Advancements. New migrations.
    Keep in mind, this is not prod and will require changes to be usable on prod.

---
+++ compyter_stats/compyterstats/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+import datetime
+from django.utils import timezone
+# Create your models here.
+
+class Computer(models.Model):
+    name = models.CharField(max_length=30)
+    def __str__(self):
+        return self.name
+
+class Timestamp(models.Model):
+    datetime_captured = models.DateTimeField('date captured', primary_key=True)
+    avg_cpu_usage=models.FloatField('overall cpu usage')
+    virtual_memory_usage=models.FloatField('overall memory usage')
+    virtual_memory_capacity=models.FloatField('max memory capacity')
+    disk_usage=models.FloatField('disk usage')
+    disk_capacity=models.FloatField('disk capacity')
+    computer=models.ForeignKey('computer', on_delete=models.CASCADE)
+
+
+class Process(models.Model):
+    origin_timestamp=models.ForeignKey('Timestamp', on_delete=models.CASCADE)
+    pid=models.IntegerField('process id')
+    cpu_usage=models.FloatField('cpu usage per process')
+    name=models.CharField(max_length=30)
+    io_counter=models.CharField(max_length=30)
+    memory_data=models.FloatField()
+    virtual_memory_data=models.FloatField()
+
+    
